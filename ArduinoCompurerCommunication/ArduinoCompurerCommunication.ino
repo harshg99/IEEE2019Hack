8b282e2ef0b7003b653b475eacd9ba447cd3f674
hydrargyrum5799 <hgoel1000p@gmail.com>
2022-12-20 14:07:23 -0500

    updated repository

---
+++ ArduinoCompurerCommunication/ArduinoCompurerCommunication.ino
@@ -0,0 +1,154 @@
+#include <LiquidCrystal.h>
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+String commandString = "";
+
+#define DOWN 5
+#define LEFT 4
+#define RIGHT 6
+#define UP 3
+
+int sensorPin =A0;
+int readU=0;
+int readD=0;
+int readL=0;
+int readR=0;
+int analogval=0;
+boolean isConnected = false;
+
+LiquidCrystal lcd(7,8,9,10,11,12); 
+
+
+void setup() {
+  
+  Serial.begin(9600);
+  pinMode(UP,INPUT);
+  pinMode(DOWN,INPUT);
+  pinMode(LEFT,INPUT);
+  pinMode(RIGHT,INPUT);
+  initDisplay();
+}
+
+void loop() {
+
+if(stringComplete)
+{
+  stringComplete = false;
+  getCommand();
+  
+  if(commandString.equals("STAR"))
+  {
+    lcd.clear();
+  }
+  if(commandString.equals("STOP"))
+  {
+    lcd.clear();
+    lcd.print("Ready to connect");    
+  }
+  else if(commandString.equals("TEX1"))
+  {
+    String text = getTextToPrint();
+    printText(text,0);
+  }
+  else if(commandString.equals("TEX2"))
+  {
+    String text = getTextToPrint();
+    printText(text,1);
+  }
+  
+  inputString = "";
+}
+button_detect();
+analogval= analogRead(sensorPin)/5.0*1024;
+if(analogval<800)
+{
+  
+}
+}
+
+void initDisplay()
+{
+  lcd.begin(16, 2);
+  lcd.setCursor(0,0);
+  lcd.clear();
+  lcd.setCursor(0,1);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Ready to connect");
+}
+
+void button_detect(){
+  readU=digitalRead(UP);
+  readR=digitalRead(RIGHT);
+  readD=digitalRead(DOWN);
+  readL=digitalRead(LEFT);
+  if(readU==LOW && readR==HIGH && readD==HIGH && readL==HIGH){
+    Serial.println("UP");
+  }
+  else if(readU==HIGH && readR==HIGH && readD==HIGH && readL==LOW){
+    Serial.println("LEFT");
+  }
+  else if(readU==HIGH && readR==LOW && readD==HIGH && readL==HIGH){
+    Serial.println("RIGHT");
+  }
+  else if(readU==HIGH && readR==HIGH && readD==LOW && readL==HIGH){
+    Serial.println("DOWN");
+  }
+  else if(readU==HIGH && readR==HIGH && readD==HIGH && readL==HIGH){
+   // Serial.println("ÄLLHIGH");
+  }
+  else{
+   Serial.println("UNDEF");
+  }
+  readU=0;
+  readR=0;
+  readD=0;
+  readL=0;
+}
+
+void getCommand()
+{
+  if(inputString.length()>0)
+  {
+     commandString = inputString.substring(1,5);
+  }
+}
+
+
+
+String getTextToPrint()
+{
+  String value = inputString.substring(5,inputString.length()-2);
+  return value;
+}
+
+void printText(String text,int cursor)
+{
+  lcd.clear();
+  lcd.setCursor(0,cursor);
+    if(text.length()<16)
+    {
+      lcd.print(text);
+    }else
+    {
+      lcd.print(text.substring(0,16));
+      lcd.setCursor(0,1);
+      lcd.print(text.substring(16,32));
+    }
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
+
---
+++ Remote/Remote.ino
@@ -0,0 +1,122 @@
+
+
+
+#define DOWN 5
+#define LEFT 4
+#define RIGHT 6
+#define UP 3
+int sensorPin=  A0;
+
+int readU=0;
+int readD=0;
+int readL=0;
+int readR=0;
+int analogval=0;
+bool flag=true;
+//0705111601
+
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 7, en = 8, d4 = 9, d5 = 10, d6 = 11, d7 = 12;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(UP,INPUT);
+  pinMode(DOWN,INPUT);
+  pinMode(LEFT,INPUT);
+  pinMode(RIGHT,INPUT);
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Control Panel");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  readU=digitalRead(UP);
+  readR=digitalRead(RIGHT);
+  readD=digitalRead(DOWN);
+  readL=digitalRead(LEFT);
+  /*if(readU==HIGH && readR==LOW && readD==LOW && readL==LOW){
+    Serial.println("UP");
+  }
+  else if(readU==LOW && readR==LOW && readD==LOW && readL==HIGH){
+    Serial.println("LEFT");
+  }
+  else if(readU==LOW && readR==HIGH && readD==LOW && readL==LOW){
+    Serial.println("RIGHT");
+  }
+  else if(readU==LOW && readR==LOW && readD==HIGH && readL==LOW){
+    Serial.println("DOWN");
+  }
+  else if(readU==LOW && readR==LOW && readD==LOW && readL==LOW){
+  }
+  else{
+    Serial.println("UNDEF");
+  }*/
+  if(analogval<100){
+    flag=true;
+    lcd.setCursor(0, 0);
+    lcd.print("Control Panel");
+  if(readU==LOW && readR==HIGH && readD==HIGH && readL==HIGH){
+    
+    lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+    lcd.print("Moving Up");
+  }
+  else if(readU==HIGH && readR==HIGH && readD==HIGH && readL==LOW){
+    lcd.setCursor(0, 1);
+     lcd.print("Moving LEFT");
+  }
+  else if(readU==HIGH && readR==LOW && readD==HIGH && readL==HIGH){
+    lcd.setCursor(0, 1);
+    lcd.print("Moving RIGHT");
+  }
+  else if(readU==HIGH && readR==HIGH && readD==LOW && readL==HIGH){
+    lcd.setCursor(0, 1);
+    lcd.print("Moving DOWN");
+  }
+  else if(readU==HIGH && readR==HIGH && readD==HIGH && readL==HIGH){
+   // Serial.println("ÄLLHIGH");
+  }
+  else{
+   //Serial.println("UNDEF");
+  }
+  Serial.println(readU);
+  Serial.println(readD);
+  Serial.println(readL);
+  Serial.println(readR);
+  Serial.println("\n\n\n");
+  delay(100);
+  readU=0;
+  readR=0;
+  readD=0;
+  readL=0;
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  //lcd.clear();
+  lcd.print("                        ");
+  }
+  analogval= 1.0*(analogRead(sensorPin));
+  Serial.println(analogval);
+  if(analogval>100 && flag)
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("First Question");
+  delay(200);
+  // print the number of seconds since reset:
+  lcd.clear();
+  lcd.print("2+3?");
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  //lcd.clear();
+  lcd.print("4 5 6 7");
+  delay(200);
+  flag=false;
+}
+}
+
